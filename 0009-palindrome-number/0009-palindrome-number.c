91d1d36426297f7afcb2ccc46c2e139d694fcaa7
Pitta Shankumar <shankumarpitta714@gmail.com>
2026-07-01 18:39:18 +0530

    Time: 4 ms (26.85%), Space: 8.8 MB (64.68%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,13 @@
+bool isPalindrome(int x) {
+    int temp=x;
+    long long rev=0;
+    if(x<0){
+        return false;
+    }else{
+    while(temp!=0){
+        rev=(rev*10)+(temp%10);
+        temp/=10;
+    }
+    }
+    return (x==rev);
+}

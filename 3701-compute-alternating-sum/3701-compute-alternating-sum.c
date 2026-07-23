0c6adc30779e73ac2dc65dfc49c61d1ed38d1faa
Pitta Shankumar <shankumarpitta714@gmail.com>
2026-07-23 14:41:04 +0530

    Time: 0 ms (100%), Space: 10.2 MB (35.04%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.c
@@ -0,0 +1,11 @@
+int alternatingSum(int* nums, int n) {
+    int sum=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                sum+=nums[i];
+            }else{
+                sum-=nums[i];
+            }
+        }
+        return sum;
+}
